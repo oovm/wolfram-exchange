0c696ca1f96391666f0b4dfbcc952a33f696f14d
Aster <galaster@foxmail.com>
2020-09-06 11:01:03 +0800

    🎀 Update wolfram version to 2020.7.7

--- projects/wolfram-lib/src/utils/system_symbols.m
+++ projects/wolfram-lib/src/utils/system_symbols.m
@@ -1,11 +1,15 @@
-Needs["JLink`"];
-InstallJava[];
-LoadJavaClass["java.awt.Toolkit", AllowShortContext -> False];
+(* ::Package:: *)
 
-CopyAsUTF8[s_String] := JavaBlock[java`awt`Toolkit`getDefaultToolkit[]@getSystemClipboard[]@setContents[#, #]&@JavaNew["java.awt.datatransfer.StringSelection", s]];
+SetDirectory@NotebookDirectory[];
+(*Needs["JLink`"];*)
+(*InstallJava[];*)
+(*LoadJavaClass["java.awt.Toolkit", AllowShortContext -> False];*)
+
+(*CopyAsUTF8[s_String] := JavaBlock[java`awt`Toolkit`getDefaultToolkit[]@getSystemClipboard[]@setContents[#, #]&@JavaNew["java.awt.datatransfer.StringSelection", s]];*)
 systems = Names["System`*"];
-TemplateApply[
-	"pub const SYSTEM_SYMBOLS: [&str; `2`] = [\"`1`\"]",
-	{StringRiffle[systems, "\",\""], Length@systems}
-]
-% // CopyAsUTF8;
+out = TemplateApply[
+	"/// `3`\npub const SYSTEM_SYMBOLS: [&str; `2`] = [\"`1`\"];",
+	{StringRiffle[systems, "\",\""], Length@systems, $Version}
+];
+(*% // CopyAsUTF8*)
+Export["systems.rs", out, "Text"]
